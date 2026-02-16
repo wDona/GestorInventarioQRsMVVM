4408b952c3758f12b69136371b2db57f42e37c41
miaudona <donanferyt@gmail.com>
2026-02-16 12:39:55 +0100

    chore: rellenar los modelos

--- app/src/main/java/dev/wdona/gestorinventarioqr/domain/model/Estanteria.java
+++ app/src/main/java/dev/wdona/gestorinventarioqr/domain/model/Estanteria.java
@@ -1,5 +1,38 @@
 package dev.wdona.gestorinventarioqr.domain.model;
 
-public class Estanteria {
+import java.util.List;
 
+public class Estanteria {
+    private String id;
+    private String nombre;
+    private List<Producto> productos;
+
+    public Estanteria(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
--- app/src/main/java/dev/wdona/gestorinventarioqr/domain/model/Producto.java
+++ app/src/main/java/dev/wdona/gestorinventarioqr/domain/model/Producto.java
@@ -1,5 +1,48 @@
 package dev.wdona.gestorinventarioqr.domain.model;
 
 public class Producto {
+    private String nombre;
+    private double precio;
+    private int cantidad;
 
+    private Estanteria estanteria;
+
+    public Producto(String nombre, double precio, int cantidad, Estanteria estanteria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.estanteria = estanteria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Estanteria getEstanteria() {
+        return estanteria;
+    }
+
+    public void setEstanteria(Estanteria estanteria) {
+        this.estanteria = estanteria;
+    }
 }
