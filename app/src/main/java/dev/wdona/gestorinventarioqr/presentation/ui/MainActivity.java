d1f2b297492f914a312d63d8fb9dc27e29f9d972
miaudona <donanferyt@gmail.com>
2026-02-17 14:01:35 +0100

    comment: TODOs para la siguiente jornada

--- app/src/main/java/dev/wdona/gestorinventarioqr/presentation/ui/MainActivity.java
+++ app/src/main/java/dev/wdona/gestorinventarioqr/presentation/ui/MainActivity.java
@@ -8,10 +8,17 @@ import dev.wdona.gestorinventarioqr.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: MOCKEAR BASE DE DATOS CON 3 ESTANTERIAS Y 5 PRODUCTOS, CON SUS CANTIDADES
+    // TODO: Pantalla principal con botones para escanear codigo QR de producto y estanteria
+    // TODO: Pantalla de escaneo de codigo QR, con opciones para agregar, quitar o asignar producto a estanteria
+    // TODO: Pantalla de consulta de estanteria, mostrando los productos que hay en esa estanteria y sus cantidades
+    // TODO: Pantalla de historial de operaciones, mostrando las operaciones realizadas y las pendientes (si se actua offline)
+    // TODO: Implementar funcionalidad de escaneo de codigo QR, utilizando una libreria como ZXing o ML Kit
+
     // TODO: Lectura de codigo QR
     // TODO: Base de datos mockeada con productos y sus cantidades
-    // TODO: Al escanear un codigo QR de producto - anadir, quitar y mover/asignar a estanteria
     // TODO: Al escanear un QR de estanteria - consultar que productos hay en esa estanteria
+    // TODO: Al escanear un codigo QR de producto - anadir, quitar y mover/asignar a estanteria
     //      FACILITAR ESCANEO DE ESTANTERIA - ESCANEO PRODUCTO - ESCANEO CANTIDAD - CONFIRMAR
     // TODO: Registrar operaciones localmente para mostrar historial y pendientes
     //      PARA PENDIENTES, "DESACTIVAR" MOCK DE BASE DE DATOS "ONLINE" Y ACTUAR OFFLINE
